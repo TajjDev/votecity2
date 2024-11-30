5274730bc29c1ff141d3f924253b7a25b3c5d5d2
Tajudeen khabab Akanbi <152615493+TajjDev@users.noreply.github.com>
2024-11-30 19:12:12 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 // import GetStarted from './assets/component/get started/GetStarted'
 import About from './assets/component/about/About'
 // import Nav from './assets/component/nav/Nav'
-import Aboutt from './assets/component2/about us/Aboutt'
+// import Aboutt from './assets/component2/about us/Aboutt'
 import Features from './assets/component2/features/Features'
 // import Features from './assets/component2/features/Features'
 // impor
